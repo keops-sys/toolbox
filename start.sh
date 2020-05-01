ebe2c8fe903efeb10c254453f800f6e6113bc7cd
k3op <admin@k3op.com>
2020-05-01 03:57:46 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+DOCKER_CONTENT_TRUST=1 docker run -d --name dd-agent -v /var/run/docker.sock:/var/run/docker.sock:ro -v /proc/:/host/proc/:ro -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro -e DD_API_KEY=38539311d549a672804f13872c592d32 datadog/agent:7
+
 cd "$(dirname "$0")"
 
 git pull
